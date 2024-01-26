8975f82632e7a0a564093c4a6d38fe7c078470e5
hodanabavi <121904092+11hoda11@users.noreply.github.com>
2024-01-26 09:46:16 -0500

    Update data processing.sql

--- data processing.sql
+++ data processing.sql
@@ -1,3 +1,8 @@
+
+-- Author: Hoda Nabavi
+-- Date: December 31, 2022
+
+
 -- Comparing successful and unsuccessful campaigns regarding goal amount
 SELECT  avg(goal) as mean, stddev(goal) as std
 FROM  kickstarter.campaign cam
@@ -5,6 +10,7 @@ where cam.outcome != 'successful' and goal<=5000000;
 SELECT  avg(goal) as mean, stddev(goal) as std
 FROM  kickstarter.campaign cam
 where cam.outcome = 'successful';
+
 
 -- Top 3 country in terms of backers and pledged
 SELECT cam.id, cam.pledged , cam.backers  , cam.goal, outcome, cou.name
